5d45a1b315e37849cd4382782b761e963173eba9
pvtl-pre <>
2022-11-04 14:54:08 -0500

    Added scan policies

--- tap/azure/scripts/tap-dev-namespace-install.sh
+++ tap/azure/scripts/tap-dev-namespace-install.sh
@@ -8,6 +8,7 @@ INSTALL_REGISTRY_USERNAME=$(yq e .tap_install.registry.username $PARAMS_YAML)
 INSTALL_REGISTRY_PASSWORD=$(yq e .tap_install.registry.password $PARAMS_YAML)
 INSTALL_DEV_NAMESPACE=$(yq e .tap_install.dev_namespace $PARAMS_YAML)
 TAP_REGISTRY_SECRET_NAME=$(yq e .tap_install.registry.secret $PARAMS_YAML)
+SCAN_POLICY=$(yq e .tap_values.scanning.source.policy $PARAMS_YAML)
 
 echo "## Add read/write registry credentials to the developer namespace"
 
@@ -115,4 +116,65 @@ subjects:
   - kind: Group
     name: GROUP-FOR-APP-EDITOR
     apiGroup: rbac.authorization.k8s.io
+EOF
+
+echo "## Create a scan policy to the developer namespace ($INSTALL_DEV_NAMESPACE)"
+
+cat <<EOF | kubectl -n $INSTALL_DEV_NAMESPACE apply -f -
+apiVersion: scanning.apps.tanzu.vmware.com/v1beta1
+kind: ScanPolicy
+metadata:
+  name: $SCAN_POLICY
+  labels:
+    'app.kubernetes.io/part-of': 'enable-in-gui'
+spec:
+  regoFile: |
+    package main
+
+    # Accepted Values: "Critical", "High", "Medium", "Low", "Negligible", "UnknownSeverity"
+    notAllowedSeverities := ["Critical", "High", "UnknownSeverity"]
+    ignoreCves := []
+
+    contains(array, elem) = true {
+      array[_] = elem
+    } else = false { true }
+
+    isSafe(match) {
+      severities := { e | e := match.ratings.rating.severity } | { e | e := match.ratings.rating[_].severity }
+      some i
+      fails := contains(notAllowedSeverities, severities[i])
+      not fails
+    }
+
+    isSafe(match) {
+      ignore := contains(ignoreCves, match.id)
+      ignore
+    }
+
+    deny[msg] {
+      comps := { e | e := input.bom.components.component } | { e | e := input.bom.components.component[_] }
+      some i
+      comp := comps[i]
+      vulns := { e | e := comp.vulnerabilities.vulnerability } | { e | e := comp.vulnerabilities.vulnerability[_] }
+      some j
+      vuln := vulns[j]
+      ratings := { e | e := vuln.ratings.rating.severity } | { e | e := vuln.ratings.rating[_].severity }
+      not isSafe(vuln)
+      msg = sprintf("CVE %s %s %s", [comp.name, vuln.id, ratings])
+    }
+EOF
+
+echo "## Create an ootb_supply_chain_testing_scanning pipeline ($INSTALL_DEV_NAMESPACE)"
+
+cat <<EOF | kubectl -n $INSTALL_DEV_NAMESPACE apply -f -
+apiVersion: tekton.dev/v1beta1
+kind: Pipeline
+metadata:
+  name: developer-defined-tekton-pipeline
+  labels:
+    apps.tanzu.vmware.com/pipeline: test      # (!) required
+spec:
+  params:
+    - name: source-url                        # (!) required
+    - name: source-revision                   # (!) required
 EOF
