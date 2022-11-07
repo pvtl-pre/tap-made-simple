97035383ac07cf8e8512e2abd039ec6f8fbc790d
pvtl-pre <>
2022-11-07 15:34:36 -0600

    Added java testing section to the Tekton pipeline

--- tap/azure/scripts/tap-dev-namespace-install.sh
+++ tap/azure/scripts/tap-dev-namespace-install.sh
@@ -177,4 +177,22 @@ spec:
   params:
     - name: source-url                        # (!) required
     - name: source-revision                   # (!) required
+  tasks:
+    - name: test
+      params:
+        - name: source-url
+          value: \$(params.source-url)
+        - name: source-revision
+          value: \$(params.source-revision)
+      taskSpec:
+        params:
+          - name: source-url
+          - name: source-revision
+        steps:
+          - name: test
+            image: gradle
+            script: |-
+              cd \`mktemp -d\`
+              wget -qO- \$(params.source-url) | tar xvz -m
+              ./mvnw test
 EOF
