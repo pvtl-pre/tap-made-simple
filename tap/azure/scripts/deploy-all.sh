f0d871bfc3bbb344789d79a2af6138bfb87149d4
pvtl-pre <>
2022-11-07 15:32:36 -0600

    Fixed script order since metadata story auth is required in the tap-values file

--- tap/azure/scripts/deploy-all.sh
+++ tap/azure/scripts/deploy-all.sh
@@ -9,6 +9,6 @@ $TKG_LAB_SCRIPTS/02-deploy-azure-container-registry.sh
 $TKG_LAB_SCRIPTS/03-deploy-azure-k8s-cluster.sh
 
 $TKG_LAB_SCRIPTS/tap-prereqs-install.sh
+$TKG_LAB_SCRIPTS/tap-metadata-store-install.sh
 $TKG_LAB_SCRIPTS/tap-profiles-install.sh
 $TKG_LAB_SCRIPTS/tap-dev-namespace-install.sh
-$TKG_LAB_SCRIPTS/tap-metadata-store-install.sh
