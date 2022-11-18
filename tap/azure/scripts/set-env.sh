7890524908b6c7db4c0f51e32e365b2b60ee556d
pvtl-pre <>
2022-11-18 10:59:16 -0600

    Added better spacing and support for multiline text to display

--- tap/azure/scripts/set-env.sh
+++ tap/azure/scripts/set-env.sh
@@ -3,14 +3,15 @@ set -e -o pipefail
 
 : ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
 
-# Give some information timestamps to know how long things take
-echo $(date)
-
 information () {
   RED='\033[0;31m'
   NO_COLOR='\033[0m'
 
   echo -e "${RED}##############################################################################"
-  echo -e "## $@"
+  echo -e "$@"
   echo -e "##############################################################################${NO_COLOR}"
+  echo ""
 }
+
+# Give some information timestamps to know how long things take
+information $(date)
