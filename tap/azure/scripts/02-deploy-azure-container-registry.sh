4d9186dc625b89145d528bf204beb5bcae47166f
pvtl-pre <>
2022-11-18 10:58:18 -0600

    Changed string bool comparisons to bool comparisons
    Added resource group to az commands because az command is wrong sometimes
    
    Changed yq usage to jq when getting a bool value

--- tap/azure/scripts/02-deploy-azure-container-registry.sh
+++ tap/azure/scripts/02-deploy-azure-container-registry.sh
@@ -5,35 +5,34 @@ shopt -s nocasematch;
 TKG_LAB_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$TKG_LAB_SCRIPTS/set-env.sh"
 
-export ACR_NAME=$(yq e .azure.acr_name $PARAMS_YAML)
-
 RESOURCE_GROUP=$(yq e .azure.resource_group $PARAMS_YAML)
+export ACR_NAME=$(yq e .azure.acr_name $PARAMS_YAML)
 ACR_SKU=$(yq e .azure.acr_sku $PARAMS_YAML)
 CREATE_ACR=false
 
 if [[ -z "$ACR_NAME" || "$ACR_NAME" == "null" ]]; then
-  export ACR_NAME=tapregistry$(date +%Y%m%d%H%M%S)
+  ACR_NAME=tapregistry$(date +%Y%m%d%H%M%S)
 
   CREATE_ACR=true
 else
-  ACR_EXISTS=$(az acr list -o json | jq ".[] | contains({name: \"$ACR_NAME\"})")
+  ACR_EXISTS=$(az acr list -g $RESOURCE_GROUP -o json | jq ".[] | contains({name: \"$ACR_NAME\"})")
 
-  if [[ -z "$ACR_EXISTS" || "$ACR_EXISTS" == 'false' ]]; then
+  if [[ -z "$ACR_EXISTS" || "$ACR_EXISTS" == false ]]; then
     CREATE_ACR=true
   fi
 fi
 
-if [[ "$CREATE_ACR" == 'true' ]]; then
-  RETURNED_ACR_JSON=$(az acr check-name --name $ACR_NAME -o json)
+if [[ "$CREATE_ACR" == true ]]; then
+  RETURNED_ACR_JSON=$(az acr check-name -n $ACR_NAME -o json)
 
-  if [[ $(echo "$RETURNED_ACR_JSON" | jq -r '.nameAvailable') != "true" ]]; then
+  if [[ $(echo "$RETURNED_ACR_JSON" | jq -r '.nameAvailable') == false ]]; then
     information "Azure Container Registry name $ACR_NAME is not available"
     exit 1
   fi
 
   information "Creating Azure Container Registry named $ACR_NAME"
 
-  az acr create --name $ACR_NAME --resource-group $RESOURCE_GROUP --sku $ACR_SKU
+  az acr create -n $ACR_NAME -g $RESOURCE_GROUP --sku $ACR_SKU
   yq e -i '.azure.acr_name = env(ACR_NAME)' "$PARAMS_YAML"
 else
   information "Azure Container Registry named $ACR_NAME already exists"
@@ -41,14 +40,14 @@ fi
 
 information "Getting Azure Container Registry creds"
 
-ACR_ADMIN_ENABLED=$(az acr show -n $ACR_NAME -o yaml | yq e .adminUserEnabled)
+ACR_ADMIN_ENABLED=$(az acr show -n $ACR_NAME -g $RESOURCE_GROUP -o json | jq '.adminUserEnabled')
 
-if [[ "$ACR_ADMIN_ENABLED" == "false" ]]; then
+if [[ "$ACR_ADMIN_ENABLED" == false ]]; then
   information "Enabling admin on registry $ACR_NAME"
-  az acr update -n $ACR_NAME --admin-enabled true
+  az acr update -n $ACR_NAME -g $RESOURCE_GROUP --admin-enabled true
 fi
 
-RETURNED_ACR_CREDS_JSON=$(az acr credential show --name $ACR_NAME -o json)
+RETURNED_ACR_CREDS_JSON=$(az acr credential show -n $ACR_NAME -g $RESOURCE_GROUP -o json)
 
 export REGISTRY_FQDN="$ACR_NAME.azurecr.io"
 export REGISTRY_USERNAME=$(echo "$RETURNED_ACR_CREDS_JSON" | jq -r '.username')
