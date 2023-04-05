93b4ff80b51129b78405c464d71b1e1e346ed69a
pvtl-pre <>
2023-04-05 10:47:32 -0500

    A generated params will be modified in order to not pollute the user's params

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e -o pipefail
 
-: ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
-
 information() {
   RED='\033[0;31m'
   NO_COLOR='\033[0m'
@@ -12,3 +10,14 @@ information() {
   echo -e "##############################################################################${NO_COLOR}"
   echo ""
 }
+
+: ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
+
+# Copy params file to the generated dir and use that instead
+mkdir -p generated
+
+if [[ ! -f "generated/params.yaml" ]]; then
+  cp -p $PARAMS_YAML generated/params.yaml
+fi
+
+PARAMS_YAML="generated/params.yaml"
