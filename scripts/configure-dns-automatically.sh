9992c9b661af6969e6e7151afcc7fd2add1a61b2
pvtl-pre <>
2023-04-07 08:35:03 -0500

    Added waiting for load balancer to be ready before auto configuring dns

--- scripts/configure-dns-automatically.sh
+++ scripts/configure-dns-automatically.sh
@@ -9,13 +9,12 @@ DNS_AUTO_CONFIGURE=$(yq e .azure.dns.auto_configure $PARAMS_YAML)
 DNS_ZONE_NAME=$(yq e .azure.dns.dns_zone_name $PARAMS_YAML)
 DNS_RESOURCE_GROUP=$(yq e .azure.dns.resource_group $PARAMS_YAML)
 ITERATE_CLUSTER_KUBECONFIG=$(yq e .clusters.iterate_cluster.k8s_info.kubeconfig $PARAMS_YAML)
+ITERATE_CLUSTER_NAME=$(yq e .clusters.iterate_cluster.k8s_info.name $PARAMS_YAML)
 ITERATE_CLUSTER_INGRESS_DOMAIN=$(yq e .clusters.iterate_cluster.ingress_domain $PARAMS_YAML)
 RUN_CLUSTER_COUNT=$(yq e '.clusters.run_clusters | length' $PARAMS_YAML)
+VIEW_CLUSTER_NAME=$(yq e .clusters.view_cluster.k8s_info.name $PARAMS_YAML)
 VIEW_CLUSTER_KUBECONFIG=$(yq e .clusters.view_cluster.k8s_info.kubeconfig $PARAMS_YAML)
 VIEW_CLUSTER_INGRESS_DOMAIN=$(yq e .clusters.view_cluster.ingress_domain $PARAMS_YAML)
-
-ITERATE_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy --kubeconfig $ITERATE_CLUSTER_KUBECONFIG -o json | jq -r .status.loadBalancer.ingress[0].ip)
-VIEW_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $VIEW_CLUSTER_KUBECONFIG)
 
 function update_a_record() {
   INGRESS_DOMAIN=$1
@@ -37,6 +36,25 @@ function update_a_record() {
   az network dns record-set a add-record -n $A_RECORD_NAME -a $IP_ADDRESS -z $DNS_ZONE_NAME -g $DNS_RESOURCE_GROUP
 }
 
+function wait_for_load_balancer() {
+  CLUSTER_NAME=$1
+  KUBECONFIG=$2
+  
+  information "Waiting for load balancer on cluster '$CLUSTER_NAME'"
+
+  # Purposefully not displaying any results of the kubectl command in the terminal while we wait
+  while ! kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $KUBECONFIG >/dev/null 2>&1; do sleep 2; done
+
+  # Account for the delay between service creation and ip address getting assigned so wait again
+  while [ -z $(kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $KUBECONFIG) ]; do sleep 2; done
+}
+
+wait_for_load_balancer $VIEW_CLUSTER_NAME $VIEW_CLUSTER_KUBECONFIG
+VIEW_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $KUBECONFIG)
+
+wait_for_load_balancer $ITERATE_CLUSTER_NAME $ITERATE_CLUSTER_KUBECONFIG
+ITERATE_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $KUBECONFIG)
+
 information "Autoconfiguring DNS Zone"
 
 information "Configuring View Cluster"
@@ -52,7 +70,9 @@ for ((i = 0; i < $RUN_CLUSTER_COUNT; i++)); do
   RUN_CLUSTER_NAME=$(yq e .clusters.run_clusters[$i].k8s_info.name $PARAMS_YAML)
 
   RUN_CLUSTER_INGRESS_DOMAIN=$(yq e .clusters.run_clusters[$i].ingress_domain $PARAMS_YAML)
-  RUN_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy --kubeconfig $RUN_CLUSTER_KUBECONFIG -o json | jq -r .status.loadBalancer.ingress[0].ip)
+
+  wait_for_load_balancer $RUN_CLUSTER_NAME $RUN_CLUSTER_KUBECONFIG
+  RUN_CLUSTER_INGRESS_IP=$(kubectl get service -n tanzu-system-ingress envoy -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --kubeconfig $KUBECONFIG)
 
   information "Configuring Run Cluster '$RUN_CLUSTER_NAME'"
 
