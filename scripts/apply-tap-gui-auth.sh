44542033486f7dbcf66a551f11e35f83dd3ee294
pvtl-pre <>
2023-08-25 09:16:46 -0500

    Updated tap gui auth script for gitops

--- scripts/apply-tap-gui-auth.sh
+++ scripts/apply-tap-gui-auth.sh
@@ -4,15 +4,18 @@ set -e -o pipefail
 SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $SCRIPTS/set-env.sh
 
+GITOPS_REPO_DIR="generated/gitops-repo"
 VIEW_CLUSTER_NAME=$(yq e .clusters.view_cluster.name $PARAMS_YAML)
 VIEW_CLUSTER_KUBECONFIG=$(yq e .clusters.view_cluster.kubeconfig $PARAMS_YAML)
 
-VIEW_PROFILE="generated/profiles/$VIEW_CLUSTER_NAME.yaml"
+VIEW_PROFILE_DIR="$GITOPS_REPO_DIR/clusters/$VIEW_CLUSTER_NAME/cluster-config/values/"
 
-information "Updating generated view profile with TAP GUI auth configuration"
+VIEW_PROFILE="$VIEW_PROFILE_DIR/tap-values.yaml"
 
-ytt -f "$PARAMS_YAML" -f $VIEW_PROFILE -f profile-overlays/tap-gui-auth.yaml --output-files generated/profiles
+information "Updating view profile with TAP GUI auth configuration"
 
-$SCRIPTS/apply-view-profile.sh
+ytt -f "$PARAMS_YAML" -f $VIEW_PROFILE -f profile-overlays/tap-gui-auth.yaml --output-files $VIEW_PROFILE_DIR
+
+$SCRIPTS/commit-gitops-repo.sh "Updating view profile with TAP GUI auth configuration"
 
 $SCRIPTS/reconcile-view-cluster.sh
