4fa9f26af2a9acb9b1135e728f32c9523ffd8318
pvtl-pre <>
2023-08-25 09:09:27 -0500

    Updated adding load balancer configuration to the profiles

--- scripts/apply-load-balancer.sh
+++ scripts/apply-load-balancer.sh
@@ -4,29 +4,33 @@ set -e -o pipefail
 SCRIPTS="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source $SCRIPTS/set-env.sh
 
+GITOPS_REPO_DIR="generated/gitops-repo"
 ITERATE_CLUSTER_NAME=$(yq e .clusters.iterate_cluster.name $PARAMS_YAML)
 RUN_CLUSTER_COUNT=$(yq e '.clusters.run_clusters | length' $PARAMS_YAML)
 VIEW_CLUSTER_NAME=$(yq e .clusters.view_cluster.name $PARAMS_YAML)
 
-ITERATE_PROFILE="generated/profiles/$ITERATE_CLUSTER_NAME.yaml"
-VIEW_PROFILE="generated/profiles/$VIEW_CLUSTER_NAME.yaml"
+ITERATE_PROFILE_DIR="$GITOPS_REPO_DIR/clusters/$ITERATE_CLUSTER_NAME/cluster-config/values/"
+VIEW_PROFILE_DIR="$GITOPS_REPO_DIR/clusters/$VIEW_CLUSTER_NAME/cluster-config/values/"
 
-information "Updating generated profiles with load balancer configuration"
+ITERATE_PROFILE="$ITERATE_PROFILE_DIR/tap-values.yaml"
+VIEW_PROFILE="$VIEW_PROFILE_DIR/tap-values.yaml"
 
-ytt -f "$PARAMS_YAML" -f $VIEW_PROFILE -f profile-overlays/load-balancer.yaml --output-files generated/profiles
-ytt -f "$PARAMS_YAML" -f $ITERATE_PROFILE -f profile-overlays/load-balancer.yaml --output-files generated/profiles
+information "Updating profiles with load balancer configuration"
+
+ytt -f "$PARAMS_YAML" -f $VIEW_PROFILE -f profile-overlays/load-balancer.yaml --output-files $VIEW_PROFILE_DIR
+ytt -f "$PARAMS_YAML" -f $ITERATE_PROFILE -f profile-overlays/load-balancer.yaml --output-files $ITERATE_PROFILE_DIR
 
 for ((i = 0; i < $RUN_CLUSTER_COUNT; i++)); do
   RUN_CLUSTER_NAME=$(yq e .clusters.run_clusters[$i].name $PARAMS_YAML)
 
-  RUN_PROFILE="generated/profiles/$RUN_CLUSTER_NAME.yaml"
+  RUN_PROFILE_DIR="$GITOPS_REPO_DIR/clusters/$RUN_CLUSTER_NAME/cluster-config/values/"
 
-  ytt -f "$PARAMS_YAML" -f $RUN_PROFILE -f profile-overlays/load-balancer.yaml --output-files generated/profiles
+  RUN_PROFILE="$RUN_PROFILE_DIR/tap-values.yaml"
+
+  ytt -f "$PARAMS_YAML" -f $RUN_PROFILE -f profile-overlays/load-balancer.yaml --output-files $RUN_PROFILE_DIR
 done
 
-$SCRIPTS/apply-view-profile.sh
-$SCRIPTS/apply-iterate-profile.sh
-$SCRIPTS/apply-run-profiles.sh
+$SCRIPTS/commit-gitops-repo.sh "Updating profiles with load balancer configuration"
 
 $SCRIPTS/reconcile-view-cluster.sh
 $SCRIPTS/reconcile-iterate-cluster.sh
